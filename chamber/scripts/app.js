3e1f483e166180f759d219ae91945a6a7d4b696c
Tarius565 <tlevi999@gmail.com>
2023-02-03 07:05:55 -0700

    banner

--- chamber/scripts/app.js
+++ chamber/scripts/app.js
@@ -5,11 +5,25 @@ navToggle.addEventListener("click", () => {
   primaryNav.toggleAttribute("data-visible");
 });
 
-var d = new Date();
-if (d.getDay() === 1) {
-  var meetGreet = document.createElement("div");
-  meetGreet.setAttribute("class", "meet-and-greet");
-  meetGreet.innerHTML =
-    "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
-  document.main.appendChild(meetGreet);
+var date = new Date();
+var year = date.getFullYear();
+document.querySelector("#copyrightYear").innerHTML = year;
+let lastModified = new Date(document.lastModified);        
+document.getElementById("dateModified").innerHTML = lastModified
+
+var dayOfWeek = date.getDay();
+
+if (dayOfWeek == 1 || dayOfWeek == 2) {
+  document.getElementById("meeting-banner").style.display = "block";
 }
+
+
+
+// var d = new Date();
+// if (d.getDay() === 1) {
+//   var meetGreet = document.createElement("div");
+//   meetGreet.setAttribute("class", "meet-and-greet");
+//   meetGreet.innerHTML =
+//     "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+//   document.main.appendChild(meetGreet);
+// }
